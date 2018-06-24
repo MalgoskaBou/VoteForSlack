d69de085b203a2a9307686ab91f3c2f68ecb730f
GM <ghouga.m@gmail.com>
2018-06-24 18:13:25 +0200

    Initial commit

---
+++ app/src/main/java/com/codingawesometeam/android/vfs/ThanksScreen.kt
@@ -0,0 +1,12 @@
+package com.codingawesometeam.android.vfs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ThanksScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thanks_screen)
+    }
+}
