c6133fd3c5fda8941393026580053b3dff54d588
Greta Gri <greta.klimaite@gmail.com>
2018-06-26 21:05:56 +0300

    Added onclick on button to go to Activity Main.

--- app/src/main/java/com/codingawesometeam/android/vfs/WelcomeScreen.kt
+++ app/src/main/java/com/codingawesometeam/android/vfs/WelcomeScreen.kt
@@ -1,7 +1,11 @@
 package com.codingawesometeam.android.vfs
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class WelcomeScreen : AppCompatActivity() {
 
@@ -9,7 +13,13 @@ class WelcomeScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
 
-
+        val intentToMain = Intent(this, MainActivity::class.java)
+        val buttonGoVote = findViewById<Button> (R.id.button_go_to_vote)
+        
+        val clickListener = View.OnClickListener { v ->
+            startActivity(intentToMain)
+            }
+        buttonGoVote.setOnClickListener(clickListener)
     }
   //  override fun onBackPressed() {
   //      alert(getString(R.string.close_app_info), getString(R.string.close_app)) {
@@ -18,4 +28,8 @@ class WelcomeScreen : AppCompatActivity() {
    //         isCancelable = false
   //      }.show()
    // }
+}
+
+private fun Int.setOnClickListener(clickListener: View.OnClickListener) {
+
 }
