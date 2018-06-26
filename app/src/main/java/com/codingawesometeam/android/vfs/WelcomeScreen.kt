fd149980c005bec3494f521d073be67d6a35abee
Greta Gri <greta.klimaite@gmail.com>
2018-06-26 20:27:18 +0300

    Added activity_Welcome screen layout, main app colors.

--- app/src/main/java/com/codingawesometeam/android/vfs/WelcomeScreen.kt
+++ app/src/main/java/com/codingawesometeam/android/vfs/WelcomeScreen.kt
@@ -8,5 +8,14 @@ class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome_screen)
+
+
     }
+  //  override fun onBackPressed() {
+  //      alert(getString(R.string.close_app_info), getString(R.string.close_app)) {
+   //         yesButton { finishAffinity() }
+  //          noButton { }
+   //         isCancelable = false
+  //      }.show()
+   // }
 }
